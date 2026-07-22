67157c246ad656c8c710c06cd29c759b17bba602
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-07-22 07:50:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //your JS code here. If required.
+let output = document.getElementById('output');
+let arr = [1,2,3,4];
+output.textContent = arr;
+
+function evenNum(arr){
+	let ans = [];
+	for(let i = 0; i < arr.length; i++){
+		if(arr[i] % 2 === 0){
+			ans.push(arr[i]);
+		}
+	}
+	return ans;
+}
+
+function doubleEvenArr(arr){
+	let ans = [];
+	for(let i = 0; i < arr.length; i++){
+		ans.push(arr[i] * 2);
+	}
+	return ans;
+}
+
+const promise1 = (data) => {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(evenNum(data));
+		},1000)
+	})
+}
+
+const promise2 = (data) => {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(doubleEvenArr(data));
+		},2000);
+	})
+}
+
+async function updateOutput(arr){
+	let p1 = await promise1(arr);
+	output.textContent = p1;
+	let p2 = await promise2(p1);
+	output.textContent = p2;
+}
+
+updateOutput(arr);
